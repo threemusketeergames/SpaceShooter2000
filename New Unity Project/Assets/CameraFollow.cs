50ee913c59581ef758adbf77c0636740a9f54ff1
Hermanoid <lucasniewohner@gmail.com>
2019-08-13 20:15:50 -0500

    Simple Camera Follow script.

---
+++ New Unity Project/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform player;
+    Vector3 relativePosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        relativePosition = transform.position - player.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.position + relativePosition;
+    }
+}
