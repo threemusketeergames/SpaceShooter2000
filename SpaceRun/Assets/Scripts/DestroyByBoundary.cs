6a10312d9f68161445e9be9336dc144f61b6ce18
saingledue <saingledue@outlook.com>
2019-09-02 14:43:58 -0500

    Made the player shoot and deestroy asteriods, but the bolts are no longer being destroyed so they fly off to infinity.

--- SpaceRun/Assets/Scripts/DestroyByBoundary.cs
+++ SpaceRun/Assets/Scripts/DestroyByBoundary.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class DestroyByBoundary : MonoBehaviour
 {
-	void OnTriggerExit (Collider other) 
-	{
-		Destroy(other.gameObject);
+    void OnTriggerExit(Collider other)
+    {
+            Destroy(other.gameObject);
 	}
 }
