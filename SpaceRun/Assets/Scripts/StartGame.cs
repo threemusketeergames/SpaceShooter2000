dc9d358b88c6cc982ee46256cb56c57054541d64
landenf <landenfogle@gmail.com>
2019-10-23 14:36:56 -0500

    WIP

---
+++ SpaceRun/Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+
+    public void ChangeScenes(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
