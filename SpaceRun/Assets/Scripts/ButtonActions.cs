b6ff2f2700d3a053f16f4aa0a685937695031a5e
landenf <landenfogle@gmail.com>
2019-10-24 13:40:01 -0500

    WIP got buttons complete

---
+++ SpaceRun/Assets/Scripts/ButtonActions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonActions : MonoBehaviour
+{
+
+    public GameObject panel;
+    public GameObject[] buttons;
+
+    public void ChangeScenes(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+    public void Instructions()
+    {
+        if(panel.active == false)
+        {
+            panel.SetActive(true);
+            for (int i = 0; i < buttons.Length ; i++)
+            {
+                buttons[i].SetActive(false);
+            }
+
+        }
+        else
+        {
+            panel.SetActive(false);
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].SetActive(true);
+            }
+        }
+    }
+}
