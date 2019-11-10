ead30f89e280de9278cdb847cce50e42150bb3da
landenf <landenfogle@gmail.com>
2019-11-10 13:49:51 -0600

    WIP

--- SpaceRun/Assets/ObjectSpawning.cs
+++ SpaceRun/Assets/ObjectSpawning.cs
@@ -8,15 +8,15 @@ public class ObjectSpawning : MonoBehaviour
     public GameObject Asteroid;
     public GameObject EnemyShip;
     public Quaternion Rotation; //Roation at spawn
-    public GameObject[] hazards; //Options
+    public GameObject[] hazards; //Options for hazards
     public int hazardCount; //How many per segment
 
-    public void SpawnForSegment()
+    public void SpawnForSegment(SpawnSegementInfo ssi)
     {
         for (int i = 0; i < hazardCount; i++)
         {
             GameObject hazard = hazards[Random.Range(0, hazards.Length)];
-          //  CurrentPoint = GizmosUtil.PointOn3DCircle(mainsegment.dir * { randomDistance}, ihat, jhat, this.GetComponent<GeometryGizmo>().innerRadius, angle);
+            CurrentPoint = GizmosUtil.PointOn3DCircle(ssi.mainSegment.dir *  Random.Range(0,ssi.mainSegment.length), ihat, jhat, this.GetComponent<GeometryGizmo>().innerRadius, angle);
             Instantiate(Asteroid, CurrentPoint, Rotation);
         }
 
