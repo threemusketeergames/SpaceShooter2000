ae61ce3c3ce0b30c5ab1662f6798068e663a716e
landenf <landenfogle@gmail.com>
2019-11-10 14:36:42 -0600

    WIP2

--- SpaceRun/Assets/ObjectSpawning.cs
+++ SpaceRun/Assets/ObjectSpawning.cs
@@ -3,21 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectSpawning : MonoBehaviour
-{
-    public Vector3 CurrentPoint; //The current point of asteroid spawining
-    public GameObject Asteroid;
-    public GameObject EnemyShip;
-    public Quaternion Rotation; //Roation at spawn
+{ 
     public GameObject[] hazards; //Options for hazards
     public int hazardCount; //How many per segment
+    public PathSpawnManager psm;
 
-    public void SpawnForSegment(SpawnSegementInfo ssi)
+    void Start()
+    {
+        psm = GetComponent<PathSpawnManager>();
+    }
+
+    public void SpawnForSegment(SpawnSegmentInfo ssi)
     {
         for (int i = 0; i < hazardCount; i++)
         {
-            GameObject hazard = hazards[Random.Range(0, hazards.Length)];
-            CurrentPoint = GizmosUtil.PointOn3DCircle(ssi.mainSegment.dir *  Random.Range(0,ssi.mainSegment.length), ihat, jhat, this.GetComponent<GeometryGizmo>().innerRadius, angle);
-            Instantiate(Asteroid, CurrentPoint, Rotation);
+            GameObject hazard = hazards[Random.Range(0, hazards.Length-1)];
+            Vector3 CurrentPoint = ssi.centerPoint + GizmosUtil.PointOn3DCircle(ssi.mainSegment.dir *  Random.Range(0,ssi.mainSegment.length), ssi.mainSegment.rightVector, ssi.mainSegment.upVector, psm.innerRadius/2f, Random.Range(0,Mathf.PI*2f));
+            Vector3 up = ssi.useWedgeAngler ? ssi.wedgeAngler.wedgePerpFromMain : ssi.mainSegment.upVector;
+            Instantiate(hazard, CurrentPoint, Quaternion.LookRotation(-ssi.mainSegment.dir));
+           // Vector3.RotateTowards()
         }
 
     }
