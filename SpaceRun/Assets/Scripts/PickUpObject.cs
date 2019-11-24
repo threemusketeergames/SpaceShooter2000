32e6831241f37a435c50ca0f0b11eaf3aff9dbe8
saingledue <saingledue@outlook.com>
2019-11-23 19:04:14 -0600

    Integrated the Main Menu Music, Game Play Music, and the Life Power Up music.

--- SpaceRun/Assets/Scripts/PickUpObject.cs
+++ SpaceRun/Assets/Scripts/PickUpObject.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(AudioSource))]
 public class PickUpObject : MonoBehaviour
 {
     public GameObject Ship;
-
+    public AudioSource Audio;
 
     void OnTriggerEnter(Collider other)
     {
@@ -13,6 +13,7 @@ public class PickUpObject : MonoBehaviour
 
         if (other.tag == "Player")
         {
+            Audio.Play();
             Ship.GetComponent<PickUpController>().DeterminPowerUp();
             Destroy(gameObject);
 
