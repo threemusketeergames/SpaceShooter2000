c3cb310f3b34fba61af7c221973a31d21d8f1040
landenf <landenfogle@gmail.com>
2020-03-20 16:03:04 -0500

    Wip. No work really done in this commit. Movement Unseeable End

--- SpaceRun/Assets/Scripts/ShipMovement.cs
+++ SpaceRun/Assets/Scripts/ShipMovement.cs
@@ -6,26 +6,32 @@ public class ShipMovement : MonoBehaviour
 {
     public float moveSpeed;
     public float rotatespeed;
-    Rigidbody rb;
+    public Rigidbody rb;
     LightSpeed lightSpeed;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         lightSpeed = GameObject.FindWithTag("GameController").GetComponent<LightSpeed>();
-       //rb.velocity = Vector3.forward * moveSpeed;
+        //rb.velocity = Vector3.forward * moveSpeed;
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (!lightSpeed.LighSpeedActive)
+        //!lightSpeed.LighSpeedActive
+        if (true)
         {
             //Turning left and right
             rb.AddRelativeTorque(Vector3.up * Input.GetAxis("Horizontal") * rotatespeed);
             rb.AddRelativeTorque(Vector3.left * Input.GetAxis("Vertical") * rotatespeed);
+            //Rotation
+            float x;
+            float z;
+            x = Input.GetAxis("Verticle") * rotatespeed;
+            z = Input.GetAxis("Horizontal") * rotatespeed;
 
+       //    rb.gameObject.transform.rotation = Quaternion.Euler(x,0,z);
             //Constant moving foward
             rb.velocity = transform.forward * moveSpeed;
 
